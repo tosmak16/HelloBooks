6c67a79cb7379580c9060c594dfc6e16d895785c
tosmak <tosmak16@gmail.com>
2017-09-06 19:08:54 +0100

    fix bugs

--- client/src/actions/searchbooks.js
+++ client/src/actions/searchbooks.js
@@ -22,21 +22,34 @@ export default function searchbooks(filterBy, searchText, data) {
     }
   }, this);
 
+  // let arraysss = ['tobi', 'tobii', 'tobi is a boy', 'the is']
+  // let c = arraysss.toString().indexOf('the')
+  // let j = arraysss.toString().slice(c, -1)
+  // let k = j.toString().indexOf(',');
+  // let l = arraysss.toString().slice(c, c + k);
+  // let p = arraysss.toString().slice(0, c).lastIndexOf(',');
+  // let u = arraysss.toString().slice(p + 1, c + k)
+  // console.log(u);
   const parseArray = newArray;
   const x = parseArray.toString().toUpperCase().search(searchText.toUpperCase());
-  const y = parseArray.toString().toUpperCase().slice(x, -1).toString().search(',');
-  let k = parseArray.toString().slice(x, x + y);
+  let j = parseArray.toString().slice(x, -1)
+  let k = j.toString().indexOf(',');
+  let l = parseArray.toString().slice(x, x + k);
+  let p = parseArray.toString().slice(0, x).lastIndexOf(',');
+  let u = parseArray.toString().slice(p + 1, x + k)
+  // const y = parseArray.toString().toUpperCase().slice(x, -1).toString().search(',');
+  // let k = parseArray.toString().slice(x, x + y);
   const sortedData = lodash.orderBy(data, 'createdAt', 'desc');
-  if (k.length === 0) {
-    k = parseArray.toString().slice(x, -1).toString();
-    let k2 = parseArray.toString().slice(-1).toString();
-    const filteredData = lodash.filter(sortedData, [filterBy, k + k2]);
+  if (l.length === 0) {
+    l = parseArray.toString().slice(x, -1).toString();
+    let l2 = parseArray.toString().slice(-1).toString();
+    const filteredData = lodash.filter(sortedData, [filterBy, l + l2]);
     return (dispatch) => {
       dispatch(getFilteredBooks(filteredData));
     }
   }
   else {
-    const filteredData = lodash.filter(sortedData, [filterBy, k]);
+    const filteredData = lodash.filter(sortedData, [filterBy, u]);
     return (dispatch) => {
       dispatch(getFilteredBooks(filteredData));
     }
