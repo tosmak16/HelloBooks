cd8da11b55e268c0a2787b59c37014e86702f77c
tosmak <tosmak16@gmail.com>
2017-09-14 19:14:46 +0100

    fix bugs

--- server/server/controllers/books.js
+++ server/server/controllers/books.js
@@ -110,7 +110,7 @@ export default {
   * @returns { object } response
   */
   deleteBooks(req, res) {
-    if (req.decoded.role === 'admin') {
+    if (req.decoded.role === 'user') {
       return res.status(403).send('Access Denied!');
     }
 
