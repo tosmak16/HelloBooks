c55c049d61e7a35de84d535e6f40117eb889f563
Andela Developer <tosmak16@gmail.com>
2017-12-12 04:04:29 +0100

    chore(webpack):add comment to webpack configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,9 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+// merge all configuration in webpack.common.js to this config
 module.exports = merge(common, {
+  // devtools helps to control if and how source maps are generated
+  // inline-source-map  transformed code are (lines only)
   devtool: 'inline-source-map',
-
 });
